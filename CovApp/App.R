4f832cf0d6d15571129156270d3ec049c661916b
AmyAmelia <amy.a.slater@gmail.com>
2017-02-27 16:35:12 +0000

    Fixes issue arising from making y axis more comprehensive, updated titles to be more informative

--- CovApp/App.R
+++ CovApp/App.R
@@ -15,17 +15,17 @@ covtidy <- read_tsv("covtidy.txt")
 # User interface: loads page with sidebar, user selects a gene from the drop down menu. 
 ui <-pageWithSidebar(
   # Application title
-  headerPanel("WES Gene Coverage"),
+  titlePanel("Horizontal coverage for WES"),
   # Sidebar- allows user to enter gene
   sidebarPanel(selectizeInput(inputId = "genename", label = "Enter HGNC gene symbol", choices = genelist, selected = NULL, multiple = FALSE, 
                              options = list(placeholder ='Gene Symbol'))),
   # main pannel will dipaly selected gene and associated coverage box plot (calculated in the server section)
-  mainPanel(h4(textOutput("caption"), align = "center"), plotOutput(outputId = "boxplot")))
+  mainPanel(h3("% of bases above 20X", align = "center"), h4(textOutput("caption"), align = "center"), plotOutput(outputId = "boxplot")))
 
 # Server: Calaculates coverage boxplot for gene specified by the user
 server <- function(input, output){output$caption <- renderText(input$genename)
   output$boxplot <- renderPlot({covtsubset <- filter(covtidy, Gene == input$genename) 
-  p <- ggplot(covtsubset, aes(x="", y="Horizontal coverage, percentage of bases above20X")) + geom_boxplot(aes(group = Gene)) + coord_cartesian(ylim = c(0, 100)) 
+  p <- ggplot(covtsubset, aes(x="", y=above20X)) + geom_boxplot(aes(group = Gene)) + coord_cartesian(ylim = c(0, 100)) 
   print(p)})}
 
 shinyApp(ui = ui, server = server)
