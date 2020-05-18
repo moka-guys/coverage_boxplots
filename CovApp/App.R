4656390926a9b32bcd668e5c4bbad12a0de7fa4d
Nana Mensah <Nana.mensah1@nhs.net>
2020-05-18 16:54:42 +0100

    Update coverage App to use latest TWIST data

--- CovApp/App.R
+++ CovApp/App.R
@@ -1,31 +1,50 @@
-#install.packages("tidyverse")
-#install.packages("stringr")
-
-
 library("shiny")
 library("tidyverse")
 library("stringr")
 
-# load files, files must be in the same directory as App.R script
-# List of genes for user selection
-genelist <- read_tsv("CovGeneSymbOct2018.txt")
-# coverage data
+# Load coverage data from the same directory as App.R
 covtidy <- read_tsv("covtidy.txt")
+genelist <- read_tsv("covtidy_genes.txt", col_names="genes")
 
-# User interface: loads page with sidebar, user selects a gene from the drop down menu. 
+# User interface
 ui <-pageWithSidebar(
   # Application title
-  titlePanel("Horizontal coverage for WES"),
-  # Sidebar- allows user to enter gene
-  sidebarPanel(selectizeInput(inputId = "genename", label = "Enter HGNC gene symbol", choices = genelist, selected = "FXYD3", multiple = FALSE, 
-                             options = list(placeholder ='Gene Symbol'))),
-  # main pannel will dipaly selected gene and associated coverage box plot (calculated in the server section)
-  mainPanel(h3("% of bases above 20X", align = "center"), h4(textOutput("caption"), align = "center"), plotOutput(outputId = "boxplot"), h6("Box plot shows 1st-3rd quartile, with the median value represented by a horizontal line. Outliers are defined as data points less than or greater than 1.5 times the interquartile range beyond the 1st and 3rd quartiles respectively, and are represented by dots. Whiskers show the range of inliers. Coverage calculated for RefSeq exonic bases +/- 5bp. N = 100 exomes (Agilent SureSelect Clinical Research Exome).")))
+  titlePanel("WES Gene Coverage"),
+  # Sidebar: allows user to select a gene from the drop down menu
+  sidebarPanel(
+    selectizeInput(
+      inputId = "genename",
+      label = "Enter HGNC gene symbol",
+      choices = genelist$genes,
+      selected = "A1BG",
+      multiple = FALSE,
+      options = list(placeholder ='Gene Symbol')
+      )
+    ),
+  # Main Panel: Display coverage box plot for a selected gene. This is calculated by the server object below.
+  mainPanel(
+    h3("Gene Coverage Boxplot:", align = "center"),
+    h4(textOutput("caption"), align = "center"),
+    plotOutput(outputId = "boxplot"),
+    p("Boxplot of in-house WES gene coverage above 20X (N=63; RefSeq exons +/- 5bp).", style="font-weight:600;text-align:center"),
+    p("Disclaimer: This plot shows the first 63 samples processed using the Twist Human Core Exome kit. Plots will be updated as more samples become available.", style="color:red;text-align:center"),
+    p("Interpreting the boxplot: The horizontal line represents the median covergae. The white box contains the 1st to 3rd quartile. Outliers are shown as dots and are defined as any data point (sample gene coverage) less than or greater than 1.5 times interquartile range. Whiskers show the range of inliers."),
+    )
+  )
 
-# Server: Calaculates coverage boxplot for gene specified by the user
-server <- function(input, output){output$caption <- renderText(input$genename)
-  output$boxplot <- renderPlot({covtsubset <- filter(covtidy, Gene == input$genename) 
-  p <- ggplot(covtsubset, aes(x="", y=above20X)) + geom_boxplot(aes(group = Gene)) + coord_cartesian(ylim = c(0, 100)) 
-  print(p)})}
+# Server: Calaculate coverage boxplot for genes specified by the user
+server <- function(input, output){
+  output$caption <- renderText(input$genename)
+  output$boxplot <- renderPlot(
+    {
+      covtsubset <- filter(covtidy, Gene == input$genename)
+      p <- ggplot(covtsubset, aes(x="", y=above20X)) +
+        geom_boxplot(aes(group = Gene)) +
+        coord_cartesian(ylim = c(0, 100)) +
+        labs(y="Bases covered above 20X (%)", x="Samples")
+      return(p)
+    }
+  )
+}
 
 shinyApp(ui = ui, server = server)
