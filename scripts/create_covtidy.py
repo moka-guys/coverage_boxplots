385ecb047fbcefdb49b9515d1fb3b7e68b962e17
Nana Mensah <Nana.mensah1@nhs.net>
2020-05-19 15:08:33 +0100

    Add HGNC symbols to covtidy_median.txt for moka datbase import

--- scripts/create_covtidy.py
+++ scripts/create_covtidy.py
@@ -5,7 +5,7 @@ Create data for covearge app from chanjo output files. Requires a file mapping H
 to Entrez Gene IDs which is available from genenames.org or from the moka.GenesHGNC_current table.
 
 Example:
-./create_covtidy.py -s genes_entrez.txt -f *.chanjo_txt
+./create_covtidy.py -s symbol_entrez_hgnc.txt -f *.chanjo_txt
 Outputs:
     covtidy.txt - Percentage coverage above 20X for each gene in each sample. Used in CovApp.
     covtidy_genes.txt - A list of unique genes in all samples. Used in CovApp.
@@ -47,12 +47,10 @@ def main():
     args = parsed_args()
     # Read files from command line
     cfiles = args.chanjo_files
-    gene_entrez = pd.read_csv(
+    symbol_gene_entrez = pd.read_csv(
         args.gene_entrez, # Entrez mapping file passed as -s on command-line.
-        delimiter='\t',
-        header=0,
-        names=['Gene','entrez'],
-        na_values=['\\N'] # Add NA strings for data exported from HeidiSQL
+        header=None,
+        names=['Gene','entrez', 'hgnc']
     )
 
     # Build table from processed chanjo outputs. Table contains sample, coverage and
@@ -60,13 +58,22 @@ def main():
     cfiles_clean = ( chanjo_transformer(cfile) for cfile in cfiles )
     cfiles_concat = pd.concat(cfiles_clean, axis=0, ignore_index=True)
 
-    # Convert entrez ids to gene symbols. This is covtidy.txt, the dataset for CovApp
+    # Convert entrez ids to gene symbols. This is covtidy.txt, a dataset for CovApp
+    gene_entrez = symbol_gene_entrez[['Gene','entrez']]
     covtidy = pd.merge(cfiles_concat, gene_entrez, how='inner', on='entrez')
-    # Get unique genes for outputs. This is covtidy_genes.txt, the second dataset for CovApp
+    # Get unique genes for outputs. This is covtidy_genes.txt, a dataset for CovApp
     covtidy_genes = pd.Series(covtidy.Gene.unique()).sort_values()
-    # Get median percentage above20X for each gene. This is covtidy_median.txt, the dataset for
-    # mokadatabase.NGSWESCoveragebyGene
-    covtidy_median = covtidy.groupby('Gene').median().reset_index().round(2)
+
+    # Get median percentage above20X for each gene.
+    # This is covtidy_median.txt, a dataset for moka NGSWESCoveragebyGene table
+    covtidy_median_long = covtidy.groupby('Gene').median()
+    covtidy_median_rounded = covtidy_median_long.reset_index().round(2)
+    covtidy_median = pd.merge(
+        covtidy_median_rounded,
+        symbol_gene_entrez[['hgnc','entrez']],
+        how='left',
+        on='entrez'
+    )
 
     # Write output files to current working directory 
     covtidy.to_csv(
@@ -77,7 +84,7 @@ def main():
         index=False
     )
     covtidy_genes.to_csv('covtidy_genes.txt', header=False, index=False)
-    covtidy_median[['Gene','above20X']].to_csv('covtidy_median.txt', header=False, index=False)
+    covtidy_median[['Gene','above20X','hgnc']].to_csv('covtidy_median.txt', header=False, index=False)
 
 if __name__ == "__main__":
     main()
